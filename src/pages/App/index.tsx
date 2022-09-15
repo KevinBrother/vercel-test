39a8705563078514de38a1de08b7ae3a14a1cba6
caojunjie <1301239018@qq.com>
2022-09-16 00:36:51 +0800

    chore:调整项目目录结构、新增start和list界面

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+
+import MenuList from '../menu-list';
+import Start from '../start';
+
+function App() {
+  return (
+    <>
+      <Start />
+      <MenuList />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/start/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { Button } from 'antd'
+
+export default function Start() {
+  const [count, setCount] = useState(0);
+  return (
+    <Button onClick={() => setCount(count + 1)}>开始吧{count}</Button>
+  )
+}
