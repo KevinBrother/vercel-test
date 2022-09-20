f762bff4c3dfcaa7156bdd151db5bdbd8ff66543
caojunjie <1301239018@qq.com>
2022-09-20 23:58:32 +0800

    feat(menu-list):简易mock数据

--- src/pages/menu-list/index.tsx
+++ src/pages/menu-list/index.tsx
@@ -1,5 +1,6 @@
 import { Table } from 'antd'
 import type { ColumnsType } from 'antd/es/table';
+import { mock } from 'mockjs';
 
 interface DataType {
   key: string;
@@ -46,8 +47,19 @@ const data: DataType[] = [
   },
 ];
 
+var mockData = mock({
+  // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+  'list|1-10': [{
+    // 属性 id 是一个自增数，起始值为 1，每次增 1
+    'key|+1': 1,
+    name: mock('@name'),
+    address: mock('@city')
+  }]
+})
+console.log('%c [ mockData ]-51', 'font-size:13px; background:pink; color:#bf2c9f;', mockData);
+
 export default function MenuList() {
   return (
-    <Table columns={columns} dataSource={data} />
+    <Table columns={columns} dataSource={mockData.list} />
   )
 }
