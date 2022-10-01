73ec38ae1d6b32472f1d2b71c4c5fd1303f3441a
caojunjie <1301239018@qq.com>
2022-10-01 18:40:59 +0800

    fix immer与mobx的问题修复

--- src/pages/category-list/hooks/useBreadcrumb.tsx
+++ src/pages/category-list/hooks/useBreadcrumb.tsx
@@ -1,22 +1,40 @@
 import { ICategory } from '@/stores';
 import { Breadcrumb } from 'antd'
+import { cloneDeep } from 'lodash-es';
+import { useState } from 'react';
 import { useImmer } from 'use-immer';
 
 export function useBreadcrumb({ setCategoryId }) {
-  const [breadCrumbs, setBreadCrumbs] = useImmer<ICategory[]>([]);
+  /* 
+    // TODO 2022年10月1日 18:37:25 为什么用immer会报mobx的错？？
+    const [breadCrumbs, setBreadCrumbs] = useImmer<ICategory[]>([]);
+    function addBreadCrumb(category: ICategory) {
+      setBreadCrumbs(draft => {
+        draft.push(category);
+      })
+    }
+    function handleClick(category: ICategory, index: number) {
+      setCategoryId(category.pId);
+      setBreadCrumbs((draft, index) => {
+        draft.splice(index);
+      });
+    }
+   */
 
+  const [breadCrumbs, setBreadCrumbs] = useState<ICategory[]>([]);
   function addBreadCrumb(category: ICategory) {
-    setBreadCrumbs(draft => {
-      draft.push(category);
-    })
+    const _breadCrumbs = cloneDeep(breadCrumbs);
+    _breadCrumbs.push(category);
+
+    setBreadCrumbs(_breadCrumbs);
   }
 
   function handleClick(category: ICategory, index: number) {
-    setCategoryId(category.pId);
+    const _breadCrumbs = cloneDeep(breadCrumbs);
+    _breadCrumbs.splice(index);
 
-    setBreadCrumbs(draft => {
-      draft.splice(index);
-    });
+    setCategoryId(category.pId);
+    setBreadCrumbs(_breadCrumbs);
   }
 
   const render = (
