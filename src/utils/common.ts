01d3a4469104fcbf6481abc5a3a8f100b9e58ed3
刘沈 <ls_20200418@163.com>
2022-10-30 19:58:22 +0800

    fix(utils/common) 随机数方法修改

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,14 +1,7 @@
 
   export const randomChoose = (list) => {
-    const shuffled = list.slice(0);
-    let i = list.length;
-    const min = i - 1;
-    let temp, index;  //只是声明变量的方式, 也可以分开写
-    while (i-- > min) {
-        index = Math.floor((i + 1) * Math.random()); //这里的+1 是因为上面i--的操作  所以要加回来
-        temp = shuffled[index];  //即值交换
-        shuffled[index] = shuffled[i]; 
-        shuffled[i] = temp;
-    }
-    return shuffled.slice(min);
+    if(!list.length) return ;
+    const l = list.length;
+    const random = Math.floor(Math.random() *l)
+   return list[random]
   }
