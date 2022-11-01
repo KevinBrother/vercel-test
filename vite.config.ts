f2017c70657ba0feb69cacee4f77b9c3e2c84cdf
caojunjie <1301239018@qq.com>
2022-11-01 14:30:57 +0800

    fix(lint) 修复lint的报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    open: true,
+    open: true
   },
   plugins: [react(), svgr(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true,
-      },
-    },
+        javascriptEnabled: true
+      }
+    }
   },
   resolve: {
     alias: [
       {
         find: /^~/,
-        replacement: '',
+        replacement: ''
       }
-
-    ],
+    ]
   }
-})
+});
